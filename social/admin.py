1efa80948743f3078bd31c0bdd25e7be1a4085b9
MisterPablito <luispeneda310@gmail.com>
2026-05-22 16:09:39 +0100

    Modelo Vulnerabilidade e admin configurado

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Vulnerabilidade
 
-# Register your models here.
+@admin.register(Vulnerabilidade)
+class VulnerabilidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'risco', 'created_at')
+    search_fields = ('nome',)
+    list_filter = ('risco',)
+    ordering = ('-created_at',)
--- social/models.py
+++ social/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Vulnerabilidade(models.Model):
+    RISCO_CHOICES = [
+        ('A', 'Alto'),
+        ('M', 'Médio'),
+        ('B', 'Baixo'),
+    ]
+
+    nome = models.CharField(max_length=100, unique=True)
+    descricao = models.TextField()
+    risco = models.CharField(max_length=1, choices=RISCO_CHOICES)
+    mitigacao = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
